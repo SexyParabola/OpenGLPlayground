64c30dd933ffcff2843d07977de1f7963d80f4da
RoBoLilly <ultrahoodsense@gmail.com>
2019-11-03 13:26:30 -0700

    Compleated CirBind Project.

--- playground.cpp
+++ playground.cpp
@@ -42,6 +42,63 @@ struct Object {
     }
 };
 
+struct Orbiter {
+	point pos;
+	point orb;
+	double radius;
+	double magnitude = 0.0f;
+	Orbiter(point Pos, double Radius) {
+		pos = Pos;
+		radius = Radius;
+		orb.x = pos.x + (cos(magnitude) * radius);
+		orb.y = pos.y + (sin(magnitude) * radius);
+	}
+	void addMag(float Mag) {
+		magnitude += Mag / 150;
+		orb.x = pos.x + (cos(magnitude) * radius);
+		orb.y = pos.y + (sin(magnitude) * radius);
+	}
+};
+
+class Rect {
+private:
+	ID id;
+	ShapeManager* sm;
+public:
+	Rect(point Pos, point Size, color Color, ShapeManager& Sm) {
+		sm  = &Sm;
+		id = sm->addRectangle(Pos, Size, Color);
+	}
+	Rect(ShapeManager& Sm) {
+		sm  = &Sm;
+		id = sm->addRectangle(point(0, 0), point(0, 0), color(0, 0 ,0));
+	}
+	const point getPos() {
+		return sm->getRectangle(id).pos;
+	}
+	const point getSize() {
+		return sm->getRectangle(id).size;
+	}
+	const color getColor() {
+		return sm->getRectangle(id).c;
+	}
+	void setPos(point Pos) {
+		rectangle temp = sm->getRectangle(id);
+		temp.pos = Pos;
+		sm->updateRectangle(id, temp);
+	}
+	void setSize(point Size) {
+		rectangle temp = sm->getRectangle(id);
+		temp.size = Size;
+		sm->updateRectangle(id, temp);
+	}
+	void setColor(color Color) {
+		rectangle temp = sm->getRectangle(id);
+		temp.c = Color;
+		sm->updateRectangle(id, temp);
+	}
+};
+
 int main( void )
 {
 	// Initialise GLFW
@@ -61,7 +118,7 @@ int main( void )
 
 	// Open a window and create its OpenGL context
 	//window = glfwCreateWindow( 2457, 1689, "Playground", NULL, NULL);
-	point windowSize(2457, 1689);
+	point windowSize(2370, 1700);
 	window = glfwCreateWindow( windowSize.x, windowSize.y, "Playground", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
@@ -84,20 +141,38 @@ int main( void )
 
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	float tickTimer = glfwGetTime();
 
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
 	ShapeManager sm;
 
-	std::vector<unsigned int> rect = sm.addRectangle(
-		point(-1.0, -1.0),
-		point(2.0, 2.0),
-		color(1, 0, 0)
-	);
+	const int xSize = 10;
+	const int ySize = 7;
 
+	float rectSize = 0.1;
 
-	//printBuffer(tm.getColorBuffer());
+	std::vector<Rect> xAxisRect;
+	for (int i = 0; i < xSize; i++) {
+		xAxisRect.push_back(Rect(sm));
+		xAxisRect[i].setSize(point(rectSize, rectSize));
+	}
+
+	std::vector<Rect> yAxisRect;
+	for (int i = 0; i < ySize; i++) {
+		yAxisRect.push_back(Rect(sm));
+		yAxisRect[i].setSize(point(rectSize, rectSize));
+	}
+
+	std::vector<std::vector<Rect>> gridRect;
+	for (int x = 0; x < xAxisRect.size(); x++) {
+		gridRect.push_back(std::vector<Rect>());
+		for (int y = 0; y < yAxisRect.size(); y++) {
+			gridRect[x].push_back(Rect(sm));
+			gridRect[x][y].setSize(point(rectSize, rectSize));
+		}
+	}
 
 	unsigned int VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
@@ -135,10 +210,18 @@ int main( void )
 	// Our ModelViewProjection : multiplication of our 3 matrices
 	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
-	float tickTimer = glfwGetTime();
-
-	Object ob(point(0, 0), point(0, 0));
-
+	const float gridXPos = -12.6;
+	const float gridYPos = 8.7;
+	const float gridSpacing = 2.5;
+	const float orbitorRadius = 1.0;
+	std::vector<Orbiter> xOrbiter;
+	for (int i = 0; i < xAxisRect.size(); i++) {
+		xOrbiter.push_back(Orbiter(point(gridXPos + (gridSpacing * (i + 1)), gridYPos), orbitorRadius));
+	}
+	std::vector<Orbiter> yOrbiter;
+	for (int i = 0; i < yAxisRect.size(); i++) {
+		yOrbiter.push_back(Orbiter(point(gridXPos, gridYPos - (gridSpacing * (i + 1))), orbitorRadius));
+	}
 	do{
 
 		const std::vector<float> vertexBuffer = sm.tm.getVertexBuffer();
@@ -167,26 +250,43 @@ int main( void )
 		// if (glfwGetTime() - tickTimer >= 0.02) {
 		// 	tickTimer = glfwGetTime();
 		// }
-		ob.acc.x = sin(glfwGetTime() * 3.0) / 100.0;
-		ob.acc.y = cos(glfwGetTime() * 3.0) / 100.0;
-		ob.tick();
-		rectangle tempRect = sm.getRectangle(rect);
-		tempRect.c = colorWheel(glfwGetTime() * 2);
-		tempRect.pos = ob.pos;
-		sm.updateRectangle(rect, tempRect);
+		for (int i = 0; i < xAxisRect.size(); i++) { /* Turns Orbitals */
+			xOrbiter[i].addMag((i)+1.0f);
+		}
+		for (int i = 0; i < yAxisRect.size(); i++) {
+			yOrbiter[i].addMag((i)+1.0f);
+		}
 
-		if (ob.pos.x >  10 * gridRatio){
-            ob.pos.x = -10 * gridRatio;
-        }
-        if (ob.pos.x < -10 * gridRatio){
-            ob.pos.x = 10 * gridRatio;
-        }
-        if (ob.pos.y > 10){
-            ob.pos.y = -10;
-        }
-        if (ob.pos.y < -10){
-            ob.pos.y = 10;
-        }
+
+		for (int i = 0; i < xAxisRect.size(); i++) { // Sets x Axis Rects assets to aline with the x Orbials
+			xAxisRect[i].setPos(xOrbiter[i].orb);
+			xAxisRect[i].setColor(colorWheel(glfwGetTime()));
+		}
+		for (int i = 0; i < yAxisRect.size(); i++) { // Sets y Axis Rects assets to aline with the y Orbials
+			yAxisRect[i].setPos(yOrbiter[i].orb);
+			yAxisRect[i].setColor(colorWheel(glfwGetTime()));
+		}
+
+
+		for (int x = 0; x < xAxisRect.size(); x++) {
+			for (int y = 0; y < yAxisRect.size(); y++) {
+				gridRect[x][y].setPos(point(xOrbiter[x].orb.x, yOrbiter[y].orb.y));
+				gridRect[x][y].setColor(colorWheel(glfwGetTime()));
+			}
+		}
+
+		// if (x1.pos.x >  10 * gridRatio){
+        //     x1.pos.x = -10 * gridRatio;
+        // }
+        // if (x1.pos.x < -10 * gridRatio){
+        //     x1.pos.x = 10 * gridRatio;
+        // }
+        // if (x1.pos.y > 10){
+        //     x1.pos.y = -10;
+        // }
+        // if (x1.pos.y < -10){
+        //     x1.pos.y = 10;
+        // }
 		//sm.updateColor(rect, colorWheel(glfwGetTime() * 2));
 
 		static bool leftPressed = false;
